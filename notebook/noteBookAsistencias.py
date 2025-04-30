41b60e8e1f4307738f978c3f88cc2cab37283426
yuliana <yuli.mejiap@gmail.com>
2025-04-29 19:56:44 -0500

    se crean reglas de filtros y conteos

--- notebook/noteBookAsistencias.py
+++ notebook/noteBookAsistencias.py
@@ -32,3 +32,41 @@ asistenciaDataFrame=pd.read_csv("../data/asistencia_estudiantes_completo.csv")
 #.head(2)                 → Muestra las 2 primeras filas reales (sin contar repeticiones).
 #print(asistenciaDataFrame['estrato'].value_counts().head(2))
 
+#Tarea: Filtros o consultas detalladas
+#1. Encontrar los estudiantes que si asistieron
+#estudiantesQueAsistieron=asistenciaDataFrame.query('estado=="asistio"')
+#print(estudiantesQueAsistieron)
+
+#2. Encontrar los estudiantes que faltaron
+#estudiantesQueFaltaron=asistenciaDataFrame.query('estado=="inasistencia"')
+#print(estudiantesQueFaltaron)
+#3. Encntrar los estudiantes que llegaron tarde(justificaron la inaxistencia)
+
+#4. Encontrar los estudiantes de estrato 1
+#estudiantesEstratoUno=asistenciaDataFrame.query('estrato==1')
+#print(estudiantesEstratoUno)
+#5. Encontrar los estudiantes de estratos altos(5 y 6)
+
+#6. Encontrar los estudiantes que llegan en metro
+#estudiantesMetro=asistenciaDataFrame.query('medio_transporte=="metro"')
+#print(estudiantesMetro)
+#7. Encontrar los estudiantes que llegaron en bicicleta
+#8. Encontrar todos los estudiantes menos los que llegaron a pie
+#estudiantesQueNoCaminan=asistenciaDataFrame.query('medio_transporte!="a pie"')
+#print(asistenciaDataFrame["medio_transporte"].unique())
+#9. Encontrar todos los registros de asistencia de junio
+#10.Encontrar todos los estudiantes que usan transportes ecologicos
+#11.Encontrar los estudiantes q usan bus y son de estrato alto 
+#12.Encontrar los estudiantes q usan bus y son de estrato bajo
+#13.Encontrar estudiantes que caminan para llegar a clases
+
+#Conteo por agrupaciones
+#1.Conteo de registros por estado de asistencia
+conteo=asistenciaDataFrame.groupby('estado').size()
+print(conteo)
+#2.Obtener el numero de registros por estrato
+#3.Cantidad de estudiantes por medio de transporte
+#4.Promedio de estrato por estado de asistencia
+#5.Maximo estrato por estado
+#6.Minimo estarto por estado
+#7.Conteo de asistencias por grupo y estado
--- notebook/noteBookUsuarios.py
+++ notebook/noteBookUsuarios.py
@@ -1,5 +1,18 @@
 import pandas as pd
+
+
 usuarioDataFrame=pd.read_excel("../data/usuarios_sistema_completo.xlsx")
 print(usuarioDataFrame)
 
 #print(usuarioDataFrame.isnull().sum)
+
+#1. Necesito una tabla con aprendices o estudiantes
+#2. Listado de instructores o profesores
+#3. Listado de especialistas en especialidad de desarrollo web o sistemas
+#4. Listado de solo usuarios con direcciones en Medellin
+#5. Listado de usuarios cuya direcciones terminen en sur
+#6. Listado de especialistas o profesores que contengan la palabra datos
+#7. Necesito docentes de Itagui
+#8. Necesito lista de nacidos del 90 o anteriores
+#9. Necesito listado instructores o profesores mayores
+#10. Necesito de profesores y estudiantes nacidos en el nuevo milenium
